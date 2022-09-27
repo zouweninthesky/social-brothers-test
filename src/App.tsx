f41c172f1649b97eaa0a00e400cb32f431cdfd79
Zouwen <zouweninthesky@yandex.ru>
2022-09-27 14:05:35 +0200

    add basic scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 import GetService from './api/GetService';
 
@@ -10,7 +8,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
