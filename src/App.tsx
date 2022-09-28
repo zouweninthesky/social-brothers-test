f5c3eecdd8ce823bf39410dec7b9a635c8483f3d
Zouwen <zouweninthesky@yandex.ru>
2022-09-28 11:24:04 +0200

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Blog from "./components/Blog/Blog";
+import Footer from "./components/common/Footer/Footer";
 
 // import GetService from './api/GetService';
 
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/blog" element={<Blog />} />
       </Routes>
+      <Footer />
     </>
   );
 }
---
+++ src/components/common/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Footer.scss";
+
+const Footer = () => {
+  return <footer className="footer">Copyright Social Brothers - 2022</footer>;
+};
+
+export default Footer;
